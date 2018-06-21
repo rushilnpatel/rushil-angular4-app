2b317e7b691b5cc7b3a2e9732ae93bbece5d69fc
rushilnpatel <rushilnpatel@gmail.com>
2018-06-21 18:40:45 -0400

    App Initial

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,6 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//import {AngularFontAwesomeModule} from 'angular-font-awesome';
+
+//PrimeNG Modules
+import {InputTextModule} from 'primeng/components/inputtext/inputtext';
+import {AccordionModule} from 'primeng/components/accordion/accordion';
+import {MenuItem} from 'primeng/components/common/api';
+import {DialogModule} from 'primeng/components/dialog/dialog';
+
+import {PanelModule} from 'primeng/panel';
+
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +23,13 @@ import { NavComponent } from './nav/nav.component';
     NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    DialogModule,
+    AccordionModule,
+    PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/nav/nav.component.ts
+++ my-app/src/app/nav/nav.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
-  selector: 'app-nav',
+  selector: 'nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
 
+  public display:boolean;
+
   constructor() { }
 
   ngOnInit() {
   }
-
+  showDialog(){
+console.log("Dialog Called...!");
+this.display=true;
+  }
 }
